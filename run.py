1044dc498dc9ba75e0a3dbfff4c5a470ab81d36b
studentofdsc <38145884+studentofdsc@users.noreply.github.com>
2020-06-08 17:09:01 -0700

    added some stuff

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ import sys
 sys.path.insert(0, f"{sys.path[0]}/src")
 from etl import *
 from data import *
+from process import *
 
 def main(targets):
     cfg = json.load(open('config/params.json'))
@@ -26,8 +27,24 @@ def main(targets):
         write_df_to_csv(df_metadata)
 
     if 'eda' in targets:
+
         books_5_genres = pd.read_csv('notebooks/metadata.csv')
         metadata = sample_genres(genres,cfg['sample_size'],books_5_genres,cfg['image_folder'],cfg['meta_folder'])
+
+        lst_rgb_hist = get_rgb_hist(metadata,cfg['image_folder'])
+        lst_img_corr = get_hist_corr(lst_rgb_hist)
+        lst_max_corr_idx = get_max_corr(lst_img_corr)
+        metadata = add_sim_book_col(metadata,lst_max_corr_idx)
+        umap_df = create_umap_rgb_hist(lst_rgb_hist,metadata)
+        lst_thumbnails = create_img_thumbnails(metadata,cfg['image_folder'],cfg['tb_folder'])
+        umap_df['thumnail_path'] = lst_thumbnails
+        create_umap_rgb_hist_with_tn(umap_df)
+
+        lst_desc,lst_kd = get_keypoints_and_desc(metadata,image_path)
+        lst_best_match = get_best_matches(lst_desc,lst_kd)
+        metadata = add_best_match_col(metadata,lst_best_match)
+        create_orb_acc_hist(metadata)
+
 
 if __name__ == '__main__':
     targets = sys.argv[1]
